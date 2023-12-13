31dacdfd3497c02a6cab74ba6230c248f541647d
Hv303 <hvdev303@gmail.com>
2023-12-13 11:48:33 +0700

    purple

--- ht hydro.lua
+++
@@ -1,7 +0,0 @@
--- Auto Harvest-Move v1.3 by Yuhkil
-local TreeID = 183 -- Farmable Seed ID
-local HarvestWorld = "GZMCU" -- World you want to harvest in.
-local ID1 = "HVRCO33" -- Door ID
-local worldname = "HJKGCRTK" -- World you want to drop the stuff in.
-local ID2 = "PELERV5" -- Door ID
-
